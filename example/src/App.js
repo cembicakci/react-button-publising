1c0dbf1cbfc3c15cf672ee969aa1f0f101792078
Cem <cmbicakci@gmail.com>
2022-07-16 20:57:48 +0300

    first commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
-import { ExampleComponent } from 'cembtnui'
+import { ExampleComponent, Button} from 'cembtnui'
 import 'cembtnui/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <>
+      <ExampleComponent text='Merhaba' />
+      <Button text='Click' onClick={() => { alert('Button') }} />
+    </>
+  )
 }
 
 export default App
